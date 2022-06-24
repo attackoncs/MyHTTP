92982c409abe68a2ca9a978d2fff16cbafe3a8cc
attackoncs <2710651793@qq.com>
2022-06-24 22:22:55 +0800

    新增Context上下文

--- gee/gee.go
+++ gee/gee.go
@@ -1,23 +1,22 @@
 package gee
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+type HandlerFunc func(c *Context)
 
 //Engine is handler for all requests
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
 func (engine *Engine) addRouter(method string, pattern string, handler HandlerFunc) {
-	engine.router[method+"-"+pattern] = handler
+	engine.router.addRouter(method, pattern, handler)
 }
 
 func (engine *Engine) GET(pattern string, handler HandlerFunc) {
@@ -36,10 +35,6 @@ func (engine *Engine) Run(addr string) (err error) {
 
 //该函数定义在http.ListenAndServe的第二个参数handler interface中，参数分别是ResponseWriter和Request
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 not found:%s\n", req.URL.Path)
-	}
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
--- gee/router.go
+++ gee/router.go
@@ -1,1 +1,28 @@
 package gee
+
+import (
+	"log"
+	"net/http"
+)
+
+type router struct {
+	handlers map[string]HandlerFunc
+}
+
+func newRouter() *router {
+	return &router{handlers: make(map[string]HandlerFunc)}
+}
+
+func (r *router) addRouter(method string, pattern string, handler HandlerFunc) {
+	log.Printf("Route %4s - %s", method, pattern)
+	r.handlers[method+"-"+pattern] = handler
+}
+
+func (r *router) handle(c *Context) {
+	key := c.Method + "-" + c.Path
+	if handler, ok := r.handlers[key]; ok {
+		handler(c)
+	} else {
+		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
+	}
+}
